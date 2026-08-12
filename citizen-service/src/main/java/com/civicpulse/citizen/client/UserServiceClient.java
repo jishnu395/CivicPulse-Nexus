4429e8684bdc31abde0b962f43955d12ace22fc5
jishnu <vjishnu744@gmail.com>
2026-08-12 11:27:12 +0530

    Fix JWT propagation for citizen service Feign calls

--- citizen-service/src/main/java/com/civicpulse/citizen/client/UserServiceClient.java
+++ citizen-service/src/main/java/com/civicpulse/citizen/client/UserServiceClient.java
@@ -1,14 +1,15 @@
 package com.civicpulse.citizen.client;
 
+import com.civicpulse.citizen.config.FeignClientConfig;
 import com.civicpulse.citizen.dto.response.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", configuration = FeignClientConfig.class)
 public interface UserServiceClient {
 
     @GetMapping("/api/users/{id}")
     UserResponse getUserById(@PathVariable Long id);
 
-}
+}
